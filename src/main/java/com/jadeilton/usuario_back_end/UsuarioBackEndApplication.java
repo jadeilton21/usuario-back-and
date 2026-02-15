c0b071647db80c0088bfb1ed42febdf84646a4c7
Jadeilton <jadeiltondm@gmail.com>
2026-02-15 19:03:50 -0300

    commit inicial

---
+++ src/main/java/com/jadeilton/usuario_back_end/UsuarioBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.jadeilton.usuario_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jadeilton/usuario_back_end/UsuarioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadeilton.usuario_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
