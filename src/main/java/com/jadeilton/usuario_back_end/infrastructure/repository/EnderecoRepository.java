edafc9da27ec51b8c10ca9b711df4f3494b06059
Jadeilton <jadeiltondm@gmail.com>
2026-02-19 15:41:46 -0300

    Faltou commitar

--- src/main/java/com/jadeilton/usuario_back_end/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/jadeilton/usuario_back_end/infrastructure/repository/EnderecoRepository.java
@@ -1,7 +1,7 @@
 package com.jadeilton.usuario_back_end.infrastructure.repository;
 
-import com.javanauta.aprendendoSpringBoot.infra.entity.Endereco;
-import com.javanauta.aprendendoSpringBoot.infra.entity.Usuario;
+
+import com.jadeilton.usuario_back_end.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
