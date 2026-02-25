83d4226b525faea8e0477f8fd3ebb7af35bd4717
Jadeilton <jadeiltondm@gmail.com>
2026-02-25 17:16:44 -0300

    Delete Class Teste

---
+++ src/test/java/com/jadeilton/usuario_back_end/UsuarioBackEndApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jadeilton.usuario_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UsuarioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
