44cb204cdd16ce5bdf062eca81bdef4a5249f7d9
Jadeilton <jadeiltondm@gmail.com>
2026-04-20 18:59:29 -0300

    Alteração no security

--- src/main/java/com/jadeilton/usuario_back_end/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/jadeilton/usuario_back_end/infrastructure/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.jadeilton.usuario_back_end.infrastructure.security;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +20,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@SecurityScheme(name = SecurityConfig.SECURITY_SCHEME, type = SecuritySchemeType.HTTP
+        , bearerFormat = "JWT", scheme = "bearer")
 public class SecurityConfig {
+
+    public static final String SECURITY_SCHEME = "bearerAuth";
+
 
     // Instâncias de JwtUtil e UserDetailsService injetadas pelo Spring
     private final JwtUtil jwtUtil;
@@ -40,6 +47,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/","/swagger-ui.html").permitAll()
                         .requestMatchers("/usuario/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/auth").permitAll()
                         .requestMatchers(HttpMethod.POST, "/usuario").permitAll()
